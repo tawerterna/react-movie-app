a5ae2b041ed11f7bff12240c4da43fc498aef8d5
tawerterna <dlmond0112@gmail.com>
2023-09-01 17:55:56 +0900

    header : css꾸미기

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,16 @@
 import { FC } from "react";
-import { Route,Routes } from "react-router-dom";
+import { Route,Routes, useLocation } from "react-router-dom";
+import ScreenHeader from "./components/header/ScreenHeader";
 import HomeContainer from "./screens/HomeScreen";
 
 const Router : FC = (props) => {
 
+    const location = useLocation();
+    const {pathname} = location;
+
     return (
         <div>
+            <ScreenHeader pathname = {pathname} />
             <Routes>
                 <Route path="/" Component={HomeContainer}></Route>  
             </Routes>
---
+++ src/components/header/ScreenHeader.tsx
@@ -0,0 +1,36 @@
+import { FC } from "react";
+import { Link } from "react-router-dom";
+import styles from './ScreenHeader.module.css';
+
+
+interface ScreenHeaderProps{
+    pathname : string;
+}
+
+const ScreenHeader : FC<ScreenHeaderProps> = ({
+    pathname = ''
+}) => {
+    return (
+        
+        <header className={styles.headerContainer}>
+            <ul className={styles.ul}> 
+             <div className = {styles.image}>
+                 <Link to = "/" className = {styles.scLink}></Link>
+             </div>
+              <li className={`${styles.li} ${pathname === '/' && styles.current}`}>
+                <Link to = "/" className = {`${styles.scLink}
+                ${pathname === '/' && styles.current} `}>
+                홈
+                </Link>
+            </li>
+            <li className={`${styles.li} ${pathname.includes('/movie') && styles.current}`}>
+                <Link to = "/movie" className = {`${styles.scLink}
+                ${pathname.includes('/movie') && styles.current} `}>
+                영희
+                </Link>
+            </li>
+            </ul>
+        </header>
+    );
+};
+export default ScreenHeader;
